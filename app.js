60b28aacf990af7af4d750669b0edb7ef2f8582c
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2015-07-01 19:53:12 -0700

    Support static assets and POST /add

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 // Import built-in modules
+
 var path = require('path');
 
 // Import NPM modules
+
+var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var express = require('express');
 
 // Init app
+
 var app = express();
 
-// Conigure app
+// Configure app
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Use middleware
 
-
-// Define routes
-app.get('/', function(req, res) {
-  res.render('index', {
-    'title': 'Hi there',
-    'items': [
-      {'desc': 'Item #1'} ,
-      {'desc': 'Item #2'}
-    ]
-  });
+// Custom logger
+app.use(function(req, res, next) {
+  console.log(req.method + ' ' + req.url + ' ' + (new Date()));
+  next();
 });
 
-app.get('/api/user/:id', function(req, res) {
-  var userId = req.params.id;
-  res.send(userId);
-});
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json()); // For parsing application/json
+app.use(bodyParser.urlencoded({ extended: true})); // For parsing application/x-www-form-urlencoded
+
+// Setup routing
+
+var todosRouter = require('./todos');
+app.use(todosRouter);
+// app.use('/api', require('api'));
 
 app.listen(3000);
