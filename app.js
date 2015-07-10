62efe4a432722a99ee422c746ca5fb92480ca832
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2015-07-10 15:00:40 -0700

    Update comment style

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// --------------
+// Imports
+// --------------
+
 // Import built-in modules
 
 var path = require('path');
@@ -8,28 +12,34 @@ var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var express = require('express');
 
-// Init app
+// --------------
+// Init and configure app
+// --------------
 
 var app = express();
-
-// Configure app
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// --------------
 // Use middleware
+// --------------
 
 // Custom logger
+
 app.use(function(req, res, next) {
   console.log(req.method + ' ' + req.url + ' ' + (new Date()));
   next();
 });
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json()); // For parsing application/json
 app.use(bodyParser.urlencoded({ extended: true})); // For parsing application/x-www-form-urlencoded
 
-// Setup routing
+// --------------
+// Routing
+// --------------
 
 var todosRouter = require('./todos');
 app.use(todosRouter);
