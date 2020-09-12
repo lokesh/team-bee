51544fb4887140e2315a747bb9bb8c099575a067
Lokesh Dhakar <lokesh.dhakar@gmail.com>
2020-09-11 21:05:50 -0700

    step 10 - middleware edits

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { indexPage, listMessages, createMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 const indexRouter = express.Router();
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', listMessages);
-indexRouter.post('/messages', createMessage);
+indexRouter.post('/messages', modifyMessage, createMessage);
 
 export default indexRouter;
